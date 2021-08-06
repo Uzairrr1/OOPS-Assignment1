e300d3616f94791cc8d479b65720553508920e25
Uzairrr1 <mohd.uzair@knoldus.com>
2021-08-06 19:41:10 +0530

    Having fun with OOPS

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "OOPSassignment1"
+
+version := "0.1"
+
+scalaVersion := "2.13.6"
---
+++ src/main/scala/SquareQueue.scala
@@ -0,0 +1,8 @@
+
+import scala.collection.mutable.ListBuffer
+
+class SquareQueue(list:ListBuffer[Int]) extends Queue {
+  for( i <- list.indices){
+    list.update(i, list(i) * list(i))
+  }
+}
